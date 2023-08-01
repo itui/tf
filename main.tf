f755394743cb69e56dfc3a60fe6f74fba414415e
itui <nebihaamirfeb2@gmail.com>
2023-08-01 17:41:04 -0400

    main.tf updated

--- main.tf
+++ main.tf
@@ -9,20 +9,22 @@ terraform {
 provider "anypoint" {
   # client_id     = "f064f7462d1948f7986fb2c77feebe5e"
   # client_secret = "ba3BA95152894695a9e0D8c169B81070"
-
-    username = "CraftSoftwareB30"              # optionally use ANYPOINT_USERNAME env var
+    username = "CraftSoftwareB30"              
     password = "Nebihaamir@123" 
 }
 
 resource "anypoint_env" "env" {
-  org_id = var.root_org_id   # environment related business group
+  # org_id = var.root_org_id   # environment related business group
+  org_id = "cba5ec70-2e53-4af3-b5c7-2eb142dc6a19"
   name = "test"                  # environment name
   type = "sandbox"              # environment type : sandbox/production
 }
 
 resource "anypoint_team" "team" {
   org_id = var.root_org_id
-  # parent_team_id = "bcb23e48-16ac-4092-b9a9-3c1c9e95a1c8"
-  parent_team_id = var.parent_team_id
+  parent_team_id = "bcb23e48-16ac-4092-b9a9-3c1c9e95a1c8"
+  # parent_team_id = var.parent_team_id
   team_name = "my_new_team_tf"
 }
+
+
